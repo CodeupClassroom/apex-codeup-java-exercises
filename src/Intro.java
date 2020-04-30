0c8e73949f1b7275c359c447a51c29fc288afc3f
Fernando Mendoza <fernandoza@gmail.com>
2020-04-30 16:49:11 -0500

    feat: half of the lecture on syntax and vars

--- src/Intro.java
+++ src/Intro.java
@@ -1,6 +1,30 @@
-public class Intro {
+ class Intro {
     // Without this method the class wouldn't run
     public static void main(String args[]){
         System.out.println("Hello Java World");
+
+        String name = "Fer";
+        byte dayOfMonth = 31;
+        short spendingLimit = 30000;
+        int carPrice = 90000;
+        long distance = 372036854775807L;
+        float grade = 93.34f;
+        double distanceToMars = 1231232354356753.34567575345345;
+        boolean isItRaining = false;
+        boolean doYouLikePizza = true;
+        char gender = 'F';
+
+        final int SIDES_OF_A_DICE = 6;
+        final String API_KEY = "b75b703d8195f6f433ca";
+        final String GITHUB_API_BASE_URL = "https://api.github.com";
+
+        System.out.println(SIDES_OF_A_DICE);
+
+        System.out.println(dayOfMonth);
+        System.out.println(spendingLimit);
+
+        System.out.println(name);
+        System.out.println("Mendoza");
+        System.out.println("Rodriguez");
     }
 }
