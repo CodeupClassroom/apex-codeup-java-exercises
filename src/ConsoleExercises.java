efd01954e7669e092236ae56b160ffdeef22f9df
Fernando Mendoza <fernandoza@gmail.com>
2020-05-01 15:38:14 -0500

    feat: almost done with the exercises, classroom measures

--- src/ConsoleExercises.java
+++ src/ConsoleExercises.java
@@ -6,21 +6,21 @@ public class ConsoleExercises {
         double pi = 3.14159;
         System.out.printf("The value of pi is approximately %.2f%n", pi);
 
-//        System.out.println("Give me a number:");
-//        int userNumber = Integer.parseInt(input.nextLine());
-//        System.out.println("userNumber = " + userNumber);
+        System.out.println("Give me a number:");
+        int userNumber = Integer.parseInt(input.nextLine());
+        System.out.println("userNumber = " + userNumber);
 
-//        String param1 = input.nextLine();
-//        String param2 = input.nextLine();
-//        String param3 = input.nextLine();
-//
-//        System.out.println("param1 = " + param1);
-//        System.out.println("param2 = " + param2);
-//        System.out.println("param3 = " + param3);
+        String param1 = input.nextLine();
+        String param2 = input.nextLine();
+        String param3 = input.nextLine();
 
-//        System.out.println("How are you feeling?");
-//        String sentence = input.nextLine();
-//        System.out.println("sentence = " + sentence);
+        System.out.println("param1 = " + param1);
+        System.out.println("param2 = " + param2);
+        System.out.println("param3 = " + param3);
+
+        System.out.println("How are you feeling?");
+        String sentence = input.nextLine();
+        System.out.println("sentence = " + sentence);
 
         System.out.println("Give me the width:");
         float width = Float.parseFloat(input.nextLine());
