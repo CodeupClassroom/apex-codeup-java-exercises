68638e2a4140635cea92391ec9b3abcf9dfa7946
Fernando Mendoza <fernandoza@gmail.com>
2020-05-05 12:07:16 -0500

    feat: create our first example of a java method

---
+++ src/MethodsLec.java
@@ -0,0 +1,11 @@
+public class MethodsLec {
+
+    public static void main(String[] args) {
+        System.out.println("sayHello() = " + sayHello());
+    }
+
+    public static String sayHello(){
+        return "Hi Fer";
+    }
+
+}
