b864c6b6d758163732d5e455e016fe24e7466070
Fernando Mendoza <fernandoza@gmail.com>
2020-05-05 12:23:36 -0500

    feat: create our first examples of a java method

--- src/MethodsLec.java
+++ src/MethodsLec.java
@@ -1,11 +1,17 @@
 public class MethodsLec {
 
     public static void main(String[] args) {
-        System.out.println("sayHello() = " + sayHello());
+        System.out.println("sum() " + sum(1, 2));
+        System.out.println("sayHello() = " + sayHello("Douglas"));
     }
 
-    public static String sayHello(){
-        return "Hi Fer";
+    public static String sayHello(String name){
+        sum(1,2);
+        return "Hi " + name;
+    }
+
+    public static int sum(int num1, int num2){
+        return num1 + num2;
     }
 
 }
