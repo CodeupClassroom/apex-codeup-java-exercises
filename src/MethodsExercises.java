7c96bd2d65c79ac52415ecd87f2e6a1f29bf9d12
Fernando Mendoza <fernandoza@gmail.com>
2020-05-06 11:44:27 -0500

    feat: first exercise completed with 4 methods

--- src/MethodsExercises.java
+++ src/MethodsExercises.java
@@ -3,10 +3,32 @@ public class MethodsExercises {
         System.out.println( 2 == sum(1,1) );
         System.out.println( 0 == sum(-1,1) );
         System.out.println( 0 != sum(-2,-5) );
+
+//        System.out.println(subtract(5,1));
+        System.out.println( 4 == subtract(5,1) );
+        System.out.println( -2 == subtract(-1,1) );
+
+        System.out.println( 5 == multiply(5,1) );
+        System.out.println( -5 == multiply(-1,5) );
+
+        System.out.println( 2 == divide(10,5) );
+        System.out.println( 2 == divide(30,15) );
     }
 
     public static double sum(double arg1, double arg2){
         return arg1 + arg2;
+    }
+
+    public static double subtract(double arg1, double arg2){
+        return arg1 - arg2;
+    }
+
+    public static double multiply(double arg1, double arg2){
+        return arg1 * arg2;
+    }
+
+    public static double divide(double arg1, double arg2){
+        return arg1 / arg2;
     }
 
 }
