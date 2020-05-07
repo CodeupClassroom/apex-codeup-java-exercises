b6702b34a90089193f21326234406516c6bbab0e
Fernando Mendoza <fernandoza@gmail.com>
2020-05-07 14:10:45 -0500

    feat: cohort example

---
+++ src/oop/Cohort.java
@@ -0,0 +1,23 @@
+package oop;
+
+public class Cohort {
+
+    private String name;
+    private String startDate;
+    public String endDate;
+    private byte studentCount;
+    public boolean graduated;
+
+    public Cohort(String name, String startDate, byte studentCount){
+        this.name = name;
+        this.startDate = startDate;
+        this.studentCount = studentCount;
+    }
+
+    protected void takeAttendance(){
+        for(byte x = 1; x <= studentCount ;x++){
+            System.out.println("Take attendance in "+this.name+": " + x);
+        }
+    }
+
+}
---
+++ src/oop/CohortApp.java
@@ -0,0 +1,19 @@
+package oop;
+
+public class CohortApp {
+    public static void main(String[] args) {
+        Cohort apex = new Cohort("Apex", "2020-02-24", (byte) 16);
+        apex.endDate = "2020-07-24";
+        String today = "july";
+
+        if(today.equals(apex.endDate)){
+            apex.graduated = true;
+        }
+
+        apex.takeAttendance();
+
+        Cohort fortuna = new Cohort("fortuna", "2020-03-24", (byte) 24);
+        fortuna.takeAttendance();
+
+    }
+}
