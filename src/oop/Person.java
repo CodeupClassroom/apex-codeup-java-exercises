5080a6d158311a5ed0e7e0d771df962e4512778c
Fernando Mendoza <fernandoza@gmail.com>
2020-05-07 13:47:35 -0500

    feat: finish the oop person example

--- src/oop/Person.java
+++ src/oop/Person.java
@@ -4,13 +4,22 @@ public class Person {
 
     public static String race = "HUMAN RACE";
 
-    public boolean isAlive;
-    public String firstName;
-    public String lastName;
+    // Creating fields
+    private String firstName;
+    protected boolean isAlive;
+    private String lastName;
+
+    // Creating rules in order to create an instance
+    // Everybody should have a First Name, Last Name and a value to know if they are alive
+    public Person(String fn, String ln, boolean isAlive){
+        this.firstName = fn;
+        this.lastName = ln;
+        this.isAlive = isAlive;
+    }
 
     public String sayHello(){
-        if(isAlive){
-            return String.format("Hello from %s %s!", firstName, lastName);
+        if(this.isAlive){
+            return String.format("Hello from %s %s!", this.firstName, this.lastName);
         } else {
             return "This person is not alive anymore :(";
         }
--- src/oop/PersonTest.java
+++ src/oop/PersonTest.java
@@ -3,20 +3,11 @@ package oop;
 public class PersonTest {
     public static void main(String[] args) {
         // Creating 2 instances for each case
-        Person fer = new Person();
-        Person ana = new Person();
-        Person deadPerson = new Person();
+        Person fer = new Person("Fernando","Mendoza R", true);
+        Person ana = new Person("Ana", "Plant", true);
+        Person deadPerson = new Person(null, null, false);
 
         System.out.println("Person.race = " + Person.race);
-
-        // Assign values to the fields
-        fer.firstName = "Fernando";
-        fer.lastName = "Mendoza R";
-        fer.isAlive = true;
-        ana.firstName = "Ana";
-        ana.lastName = "Plant";
-        ana.isAlive = true;
-        deadPerson.isAlive = false;
 
         System.out.println("ana.sayHello() = " + ana.sayHello());
         System.out.println("fer.sayHello() = " + fer.sayHello());
