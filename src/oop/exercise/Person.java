f21ef5073f976abf8d0a539ff3114f4f8abb899f
Fernando Mendoza <fernandoza@gmail.com>
2020-05-08 11:30:59 -0500

    feat: finish exercise 2

--- src/oop/exercise/Person.java
+++ src/oop/exercise/Person.java
@@ -23,6 +23,27 @@ public class Person {
     public static void main(String[] args) {
         Person fer = new Person("Fer");
         fer.sayHello();
+
+//        Person person1 = new Person("John");
+//        Person person2 = new Person("John");
+//        System.out.println(person1.getName().equals(person2.getName()));
+//        System.out.println(person1.toString());
+//        System.out.println(person2.toString());
+//        System.out.println(person1 == person2);
+
+//        Person person1 = new Person("John");
+//        Person person2 = person1;
+//        System.out.println(person1.toString());
+//        System.out.println(person2.toString());
+//        System.out.println(person1 == person2);
+
+//        Person person1 = new Person("John");
+//        Person person2 = person1;
+//        System.out.println(person1.getName());
+//        System.out.println(person2.getName());
+//        person2.setName("Jane");
+//        System.out.println(person1.getName());
+//        System.out.println(person2.getName());
     }
 
 }
