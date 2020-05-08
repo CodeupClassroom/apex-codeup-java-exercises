f8729297480d11b5ceeb681581cf594ed30d4929
Fernando Mendoza <fernandoza@gmail.com>
2020-05-08 11:22:49 -0500

    feat: finish exercise 1

--- src/oop/exercise/Person.java
+++ src/oop/exercise/Person.java
@@ -4,12 +4,25 @@ public class Person {
 
     private String name;
 
+    public Person(String aName){
+        this.setName(aName);
+    }
+
     public String getName(){
         return this.name;
     }
 
     public void setName(String aName){
         this.name = aName;
+    }
+
+    public void sayHello(){
+        System.out.println("Hello from " + this.getName());
+    }
+
+    public static void main(String[] args) {
+        Person fer = new Person("Fer");
+        fer.sayHello();
     }
 
 }
