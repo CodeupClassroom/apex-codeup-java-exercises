fbb8f68792024c2e7b2999a5831618598dfe1066
Fernando Mendoza <fernandoza@gmail.com>
2020-05-11 15:41:57 -0500

    feat: add a new rectangle class

---
+++ src/shapes/Rectangle.java
@@ -0,0 +1,13 @@
+package shapes;
+
+public class Rectangle {
+
+    protected double length;
+    protected double width;
+
+    public Rectangle(double length, double width){
+        this.length = length;
+        this.width = width;
+    }
+
+}
