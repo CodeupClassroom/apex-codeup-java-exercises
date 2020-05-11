5eef7e1277ceccd91ab45294ec153150da7770be
Fernando Mendoza <fernandoza@gmail.com>
2020-05-11 17:04:18 -0500

    feat: create the initial structure for a music player

---
+++ src/oop/review/Album.java
@@ -0,0 +1,48 @@
+package oop.review;
+
+public class Album extends PlayableItem {
+
+    private Song[] trackList;
+    private String releaseDate;
+    private String label;
+    private String name;
+
+    public Album(Song[] trackList, String releaseDate, String label, String name) {
+        this.trackList = trackList;
+        this.releaseDate = releaseDate;
+        this.label = label;
+        this.name = name;
+    }
+
+    public Song[] getTrackList() {
+        return trackList;
+    }
+
+    public void setTrackList(Song[] trackList) {
+        this.trackList = trackList;
+    }
+
+    public String getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(String releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+
+    public String getLabel() {
+        return label;
+    }
+
+    public void setLabel(String label) {
+        this.label = label;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
