7854866ac35e932792a272f40fd4b6ed80086df8
Fernando Mendoza <fernandoza@gmail.com>
2020-05-13 15:09:41 -0500

    feat: add an example of initializing an array in a constructor

---
+++ src/collections/Album.java
@@ -0,0 +1,13 @@
+package collections;
+
+import oop.review.Song;
+
+import java.util.ArrayList;
+
+public class Album {
+    private ArrayList<Song> songs;
+
+    public Album() {
+        this.songs = new ArrayList<>();
+    }
+}
