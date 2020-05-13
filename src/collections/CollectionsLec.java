02c22b5eb806c24654d0cae7de3b90e6323cc609
Fernando Mendoza <fernandoza@gmail.com>
2020-05-13 11:40:40 -0500

    feat: create an example of every method we teach in arraylist

--- src/collections/CollectionsLec.java
+++ src/collections/CollectionsLec.java
@@ -8,15 +8,33 @@ public class CollectionsLec {
 
 //        String[] names = {"fer", "stacy"};
         ArrayList<String> namesList = new ArrayList<>();
+
+        System.out.println("namesList.isEmpty() = " + namesList.isEmpty());
         namesList.add("fer");
+        System.out.println("namesList.isEmpty() = " + namesList.isEmpty());
         namesList.add("stacy");
         namesList.add("douglas");
         namesList.add("emily");
+        namesList.add("douglas");
+        namesList.add("nando");
+
+        System.out.println("namesList.contains(\"emily\") = " + namesList.contains("emily"));
+        System.out.println("namesList.contains(\"emili\") = " + namesList.contains("emili"));
+
+        System.out.println("namesList.get(0) = " + namesList.get(0));
+
+        System.out.println("namesList.indexOf(\"douglas\") = " + namesList.indexOf("douglas"));
+        System.out.println("namesList.lastIndexOf(\"douglas\") = " + namesList.lastIndexOf("douglas"));
 
         System.out.println("namesList.size() = " + namesList.size());
 
-        for (String name : namesList) {
-            System.out.println("name = " + name);
+        // removing the last index of douglas
+        namesList.remove(namesList.lastIndexOf("douglas"));
+
+        if(!namesList.isEmpty()){
+            for (String name : namesList) {
+                System.out.println("name = " + name);
+            }
         }
 
     }
