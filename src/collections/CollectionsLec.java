1a285f96c3afa9707cd50836cb620ff6ed5817c0
Fernando Mendoza <fernandoza@gmail.com>
2020-05-13 11:53:42 -0500

    feat: create a real world example of arraylist

--- src/collections/CollectionsLec.java
+++ src/collections/CollectionsLec.java
@@ -1,10 +1,14 @@
 package collections;
 
+import oop.Person;
+
 import java.util.ArrayList;
 
 public class CollectionsLec {
 
     public static void main(String[] args) {
+
+        personTest();
 
 //        String[] names = {"fer", "stacy"};
         ArrayList<String> namesList = new ArrayList<>();
@@ -35,6 +39,22 @@ public class CollectionsLec {
             for (String name : namesList) {
                 System.out.println("name = " + name);
             }
+        }
+
+    }
+
+
+    public static void personTest(){
+
+        //Person[] people = size, which props(?)
+        ArrayList<Person> people = new ArrayList<>();
+        //people[0] = new Person("Fer", "Mendoza", true);
+        people.add(new Person("Fer", "Mendoza", true));
+        people.add(new Person("Stacy", "Malibu", false));
+        people.add(new Person("Ana", "Morton", true));
+
+        for (Person person : people) {
+            System.out.println("person.getFullName() = " + person.getFullName());
         }
 
     }
