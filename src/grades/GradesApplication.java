d9228d0e4b1899b71ad6442acc9763eb83d9d692
Fernando Mendoza <fernandoza@gmail.com>
2020-05-14 09:51:52 -0500

    feat: start the cli logic

--- src/grades/GradesApplication.java
+++ src/grades/GradesApplication.java
@@ -1,15 +1,54 @@
 package grades;
 
+import util.Input;
+
+import java.util.HashMap;
+
 public class GradesApplication {
 
+    static Input input = new Input();
+
     public static void main(String[] args) {
-        Student justin = new Student("Justin");
+
+        HashMap<String, Student> students = new HashMap<>();
+
+        Student justin = new Student("Justin R");
         justin.addGrade(90);
         System.out.println(justin.getGradeAverage());
         System.out.println(justin.getGradeAverage() == 90);
         justin.addGrade(70);
         System.out.println(justin.getGradeAverage() == 80);
 
+        Student sophie = new Student("Sophie K");
+        sophie.addGrade(100);
+        sophie.addGrade(50);
+
+        students.put("jreich5", justin);
+        students.putIfAbsent("sophiek", sophie);
+
+        cli(students);
+
+    }
+
+    public static void cli(HashMap<String, Student> students){
+        System.out.println("Welcome!\n" +
+                "\n" +
+                "Here are the GitHub usernames of our students:");
+        do{
+
+            for (String key : students.keySet()) {
+                System.out.println(key + " ");
+            }
+
+            System.out.println("What student would you like to see more information on?\n");
+            String userInput = input.getString();
+            if(students.containsKey(userInput)){
+                System.out.println("found it");
+            }else{
+                System.out.println("Sorry, no student found with the GitHub username of "+ userInput);
+            }
+            System.out.println("Would you like to see another student?");
+        }while(input.yesNo());
 
     }
 
