106e9dba000662e7a546927102fc96331e3e206f
Douglas Hirsh <douglas@codeup.com>
2020-05-15 11:15:23 -0500

    fix: Handle errors in getDouble and getDouble(min, max).

--- src/util/Input.java
+++ src/util/Input.java
@@ -31,12 +31,17 @@ public class Input {
 
     public int getInt(){
         System.out.println("Give me an integer:");
-        return Integer.parseInt(scanner.nextLine());
+        return Integer.valueOf(scanner.nextLine());
     }
 
     public double getDouble(double min, double max){
         System.out.println("Give me a decimal between " + min + " and "  + max);
-        double aDecimal = Double.parseDouble(scanner.nextLine());
+        double aDecimal;
+        try {
+            aDecimal = Double.valueOf(scanner.nextLine());
+        } catch (NumberFormatException e) {
+            return getDouble(min, max);
+        }
         if(aDecimal < min || aDecimal > max){
             System.out.println("Outside of the range, try again");
             return getDouble(min, max);
@@ -46,7 +51,11 @@ public class Input {
 
     public double getDouble(){
         System.out.println("Give me a decimal:");
-        return Double.parseDouble(scanner.nextLine());
+        try {
+            return Double.valueOf(scanner.nextLine());
+        } catch (NumberFormatException e) {
+            return getDouble();
+        }
     }
 
 }
